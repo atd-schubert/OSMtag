2e44a9e8b27eb57fd5a154d47520aacde7b5213b
Arne Schubert <atd.schubert@gmail.com>
2017-12-10 11:32:07 +0100

    Changes with Falk

--- function.leaflet.php
+++ function.leaflet.php
@@ -2,7 +2,8 @@
 /*
  * OSM Plugin for CMS Made Simple
  * Author: Stefano Sabatini
- * Last rev: 28/08/2013
+ * Author: Arne Schubert
+ * Last rev: 10/12/2017
 */
 
 function lsplit($str)
@@ -30,38 +31,17 @@ function smarty_cms_function_leaflet($params, &$smarty)
 		
 		$marker[2]=str_replace('"','\"',$marker[2]);
 	}
-	
-	$geojson = isset($params['geojson']) ? $params['geojson'] : '';
-	$gpx= isset($params['gpx']) ? $params['gpx'] : '';
-	//icon
 ?>
 
-<link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.6.4/leaflet.css" />
-<!--[if lte IE 8]>
-    <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.6.4/leaflet.ie.css" />
-<![endif]-->
-
-<script src="http://cdn.leafletjs.com/leaflet-0.6.4/leaflet.js"></script>
-<script src="http://mapbox.github.io/togeojson/togeojson.js"></script>
-<script src="https://rawgithub.com/brunob/leaflet.fullscreen/master/Control.FullScreen.js"></script>
-
-<script type="text/javascript">
-//https://code.google.com/p/microajax/
-function microAjax(B,A){this.bindFunction=function(E,D){return function(){return E.apply(D,[D])}};this.stateChange=function(D){if(this.request.readyState==4){this.callbackFunction(this.request.responseText)}};this.getRequest=function(){if(window.ActiveXObject){return new ActiveXObject("Microsoft.XMLHTTP")}else{if(window.XMLHttpRequest){return new XMLHttpRequest()}}return false};this.postBody=(arguments[2]||"");this.callbackFunction=A;this.url=B;this.request=this.getRequest();if(this.request){var C=this.request;C.onreadystatechange=this.bindFunction(this.stateChange,this);if(this.postBody!==""){C.open("POST",B,true);C.setRequestHeader("X-Requested-With","XMLHttpRequest"); C.setRequestHeader("Content-type","application/x-www-form-urlencoded");C.setRequestHeader("Connection","close")}else{C.open("GET",B,true)}C.send(this.postBody)}};
-</script>
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/leaflet/1.2.0/leaflet.css" />
+<script src="//cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.3/leaflet.js"></script>
 
 <style>
 #mapdiv{
-width:<?php echo $size[0];?>;
-height:<?php echo $size[1];?>;
+  width:<?php echo $size[0];?>;
+  height:<?php echo $size[1];?>;
 }
-#mapdiv:-webkit-full-screen { width: 100% !important; height: 100% !important; }
-#mapdiv:-moz-full-screen { width: 100% !important; height: 100% !important; }
-#mapdiv:full-screen { width: 100% !important; height: 100% !important; }
 
-.leaflet-control-zoom-fullscreen {
-background-image: url(https://github.com/brunob/leaflet.fullscreen/raw/master/icon-fullscreen.png);
-}
 </style>
 <div id="mapdiv"></div>  
 
@@ -69,66 +49,26 @@ background-image: url(https://github.com/brunob/leaflet.fullscreen/raw/master/ic
 	var zoom=<?php echo $pos[0]?>;
 	var lat=<?php echo $pos[1]?>;
 	var lon=<?php echo $pos[2]?>;
-	
-	var iconurl='';
-	<?php if ($marker!==NULL):?>
-var mlat=<?php echo $marker[0]?>;
-	var mlon=<?php echo $marker[1]?>;
-	var mtxt="<?php echo $marker[2]?>";
-	<?php endif;?>
-	
+
 	var osm = new L.TileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {maxZoom: 19, attribution: 'Map Data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'});
-	var cyclemap = new L.TileLayer('http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png', {maxZoom: 18, attribution: 'Map Data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'});
-	var mapquest = new L.TileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {subdomains: '1234', maxZoom: 18, attribution: 'Map Data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'});	
+	var otm = new L.TileLayer('http://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {maxZoom: 19, attribution: 'Map Data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'});
+	var seamap = new L.TileLayer('http://t{s}.openseamap.org/seamark/{z}/{x}/{y}.png', {subdomains: '123', maxZoom: 18, attribution: 'Map Data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'});
 
 	var map = new L.Map('mapdiv', {
-    center: new L.LatLng(lat, lon),
-    zoom: zoom,
-    layers: [osm]
+      center: new L.LatLng(lat, lon),
+      zoom: zoom,
+      layers: [osm]
 	});
-	
-	var baseMaps = {
-    "Mapnik": osm,
-	"OpenCycleMap": cyclemap,
-    "Mapquest Open": mapquest	
-	};
-	L.control.layers(baseMaps).addTo(map);
 
-	var fullScreen = new L.Control.FullScreen();
-	map.addControl(fullScreen);
-	
-function loadLayer(url)
-{
-var myLayer = L.geoJson(url,{
-	onEachFeature:function onEachFeature(feature, layer) {
-		if (feature.properties && feature.properties.name) {
-			layer.bindPopup(feature.properties.name);
-		}
-	},
-    pointToLayer: function (feature, latlng) {
-        var marker=L.marker(latlng);
-        return marker;
-    }
-}).addTo(map);
-}
+    var baseMaps = {
+        "OSM": osm,
+        "OTM": otm
+    };
+    var overlayMaps = {
+        "OpenSeaMap": seamap,
+    };
+	L.control.layers(baseMaps, overlayMaps).addTo(map);
 
-	<?php if ($marker!==NULL):?>	
-	var marker = L.marker([mlat,mlon]).addTo(map);
-	if (mtxt!='') marker.bindPopup(mtxt);
-	map.addLayer(marker);
-	<?php endif;?>
-	
-<?php if ($geojson!=''):?>
-
-microAjax("<?php echo $geojson;?>",function (res) { loadLayer(JSON.parse(res)); } );
-
-<?php endif;?>
-
-<?php if ($gpx!=''):?>
-
-microAjax("<?php echo $gpx;?>",function (res) { loadLayer(toGeoJSON.gpx(res)); } );
-
-<?php endif;?>
 </script>
 <?php
 }
@@ -149,8 +89,6 @@ function smarty_cms_help_function_leaflet()
 <tr><th>Parameter</th><th>Format</th><th>Optional</th><th>Description</th></tr>
 <tr><td>size</td><td>w/h</td><td>false</td><td>Width and height (add px or %, ex 300px/300px or 100%/100%, if both height and width are percentages, height stands at 100px)</td></tr>
 <tr><td>c</td><td>z/lat/lon</td><td>false</td><td>Zoom / latitude / longitude</td></tr>
-<tr><td>m</td><td>lat/lon/text</td><td>true</td><td>To add a single marker (mind the text is optional; if contains slashes escape them with a backslash)</td></tr>
-<tr><td>geojson</td><td>url</td><td>true</td><td>Makes use of Ajax to load a geojson file</td></tr>
 </table>
 <br/>
 <p style="font-weight:bold;font-size:18px">Notes</p>
@@ -161,11 +99,12 @@ function smarty_cms_about_function_leaflet()
 {
 ?>
 <p style="font-size:24px">OpenStreetMap tag (Leaflet based) for CMS Made Simple</p>
-<p>Author Stefano Sabatini</p>
-<p>Version 0.0.3</p>
-<p>Last revision: 28/08/2013</p>
+<p>Author Stefano Sabatini, Arne Schubert with the help of Falk Zscheile</p>
+<p>Version 0.0.4</p>
+<p>Last revision: 10/12/2017</p>
 <br/>
 <p style="font-weight:bold; font-size:18px">Changelog</p>
+-0.0.4 : Remove not needed functions (for us) and use common cdns
 -0.0.3 : Fullscreen control
 -0.0.2 : Initial release (single marker and geojson)
 <?php
